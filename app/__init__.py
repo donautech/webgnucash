22c70d3715d40eb47910e8f80498037e4db55f8c
dmi3coder <dmi3coder@gmail.com>
2019-07-23 11:04:43 +0300

    added email support

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 app = Flask(__name__, static_url_path='/web')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
-from app import routes #, models
+from app import routes  # , models
